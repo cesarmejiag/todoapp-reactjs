e45b01827bd2fa719b316c61a233c8f0d248ab17
César Mejía <cesar.mejia@bancoazteca.com>
2021-10-06 16:31:55 -0500

    Example of ReactDOM.render()

--- src/index.js
+++ src/index.js
@@ -8,14 +8,21 @@ const todo = {
   text: "Study ReactJS",
 };
 
-ReactDOM.render(
-  <ul>
-    <li className="TodoItem">
-      {todo.text} - {todo.done ? "Done" : "Pending"}
-    </li>
-  </ul>,
-  document.getElementById("root")
-);
+setInterval(() => {
+  let time = new Date();
+
+  ReactDOM.render(
+    <>
+      <div>{time.toLocaleString()}</div>
+      <ul>
+        <li className="TodoItem">
+          {todo.text} - {todo.done ? "Done" : "Pending"}
+        </li>
+      </ul>
+    </>,
+    document.getElementById("root")
+  );
+}, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
